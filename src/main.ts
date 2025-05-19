fcaf10d89f7acf0138deae084ffcfb02f31ac753
davi chaves <d.chavesninomiya@gmail.com>
2025-05-19 19:00:20 -0300

    feat(Cors): enables Cors for the frontend

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,11 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors({
+    origin: 'http://localhost:4200',
+    credentials: true,
+  })
+
   const config = new DocumentBuilder()
     .setTitle('Zhukov API')
     .setDescription('API Example')
@@ -13,7 +18,7 @@ async function bootstrap() {
     .build();
   const documentFactory = () => SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api/docs', app, documentFactory());
-  
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
